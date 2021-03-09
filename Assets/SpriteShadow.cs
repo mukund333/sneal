68b24bd5dbf21c73841ebd12637460801eee1528
mukund <mukundmagdum3@gmail.com>
2021-03-10 00:53:29 +0530

    added shadow

---
+++ Assets/SpriteShadow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteShadow : MonoBehaviour
+{
+	[SerializeField]private Vector2 spriteOffset = new Vector2(-4f, -3f);
+
+	[SerializeField]private SpriteRenderer spriteToShadow;
+
+	[SerializeField]private SpriteRenderer shadowRenderer;
+
+	[SerializeField]private Sprite lastSprite;
+
+	[SerializeField]private Transform thisTransform;
+
+	[SerializeField]private Transform transformToShadow;
+	
+	private void Start()
+	{
+		this.spriteToShadow = base.transform.parent.GetComponent<SpriteRenderer>();
+		this.shadowRenderer = base.GetComponent<SpriteRenderer>();
+		this.thisTransform = base.transform;
+		this.transformToShadow = this.spriteToShadow.transform;
+		this.shadowRenderer.color = new Color(0f, 0f, 0f);
+	}
+
+	private void LateUpdate()
+	{
+		if (this.spriteToShadow.sprite != this.lastSprite)
+		{
+			this.shadowRenderer.sprite = this.spriteToShadow.sprite;
+			this.lastSprite = this.spriteToShadow.sprite;
+		}
+		this.thisTransform.position = new Vector2(this.transformToShadow.position.x + this.spriteOffset.x, this.transformToShadow.position.y + this.spriteOffset.y);
+	}
+
+
+}
+
